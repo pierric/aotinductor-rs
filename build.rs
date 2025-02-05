d715017233ed9b10fc5a815417b700b7c02c325b
Jiasen Wu <jiasenwu@hotmail.com>
2025-02-05 10:16:12 +0100

    update build

--- build.rs
+++ build.rs
@@ -2,26 +2,28 @@ use cxx_build::CFG;
 use std::path::PathBuf;
 
 fn main() {
-    if let Some(lib_path) = std::env::var_os("LIBTORCH_LIB") {
-        println!(
-            "cargo:rustc-link-arg=-Wl,-rpath={}",
-            lib_path.to_string_lossy()
-        );
-    }
+    let libtorch_path = match std::env::var_os("LIBTORCH").map(PathBuf::from) {
+        None => {
+            panic!("set env var LIBTORCH to the path to libtorch")
+        },
+        Some(p) => p,
+    };
+
+    let lib_path = std::env::var_os("LIBTORCH_LIB").map(PathBuf::from).unwrap_or(libtorch_path.join("lib"));
+    println!(
+        "cargo:rustc-link-arg=-Wl,-rpath={}",
+        lib_path.to_string_lossy()
+    );
     println!("cargo:rustc-link-arg=-Wl,--no-as-needed");
     println!("cargo:rustc-link-arg=-ltorch");
 
     // use empty prefix: https://cxx.rs/build/cargo.html#header-include-paths
     CFG.include_prefix = "";
 
-    let inc_path = std::env::var_os("LIBTORCH_INCLUDE").map(PathBuf::from);
-    if inc_path.is_none() {
-        panic!("set env var LIBTORCH_INCLUDE to the path to libtorch");
-    }
-    let torch_inc_base = inc_path.unwrap().join("include");
+    let torch_inc_base = libtorch_path.join("include");
     let torchapi_inc_path = torch_inc_base.join("torch/csrc/api/include");
 
-    cxx_build::bridge("src/aoti.rs") // returns a cc::Build
+    cxx_build::bridge("src/aoti.rs")
         .include(torch_inc_base)
         .include(torchapi_inc_path)
         .file("csrc/aoti.cc")
